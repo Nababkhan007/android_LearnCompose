668da8270e6827d195ca9d4c114d72a055b375a4
Nababkhan007 <markohenry46@gmail.com>
2021-08-12 12:45:23 +0600

    Add common text attribute

--- app/src/main/java/com/khan/learncompose/MainActivity.kt
+++ app/src/main/java/com/khan/learncompose/MainActivity.kt
@@ -3,9 +3,15 @@ package com.khan.learncompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +24,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WelcomeMessage(message: String) {
-    Text(text = "Hello, $message, Welcome Here")
+    val color: Color = if (isSystemInDarkTheme())
+        Color.White
+    else
+        Color.Black
+    Text(
+        text = "Hello, $message, Welcome Here",
+        color = color,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.SemiBold,
+        fontStyle = FontStyle.Italic,
+        fontFamily = FontFamily.Cursive,
+    )
 }
 
 @Preview
